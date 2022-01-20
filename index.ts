ab132b7c3a69ae7103e8aaff785af4f2ed245bb2
Erik Froese <froesecom@gmail.com>
2022-01-20 19:28:24 +1100

    feat: basic structure for grid and cell config

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-const defaultSquare = { possibilities: [1, 2, 3, 4, 5, 6, 7, 8, 9] }
+const SQUARE_WIDTH = 3
+const GRID_SIZE = [SQUARE_WIDTH * 3, SQUARE_WIDTH * 3]
+
+interface Cell {
+  possibilities: Number[]
+  actual: null | Number
+}
+
+const defaultCell: Cell = {
+  possibilities: [1, 2, 3, 4, 5, 6, 7, 8, 9], // This needs to be build off GRID_SIZE
+  actual: null
+}
