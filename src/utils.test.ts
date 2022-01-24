02d83a59057a2e715e504127ebad232ad41bca22
Erik Froese <froesecom@gmail.com>
2022-01-24 20:22:32 +1100

    test: test for 'row' traverse

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,9 +1,18 @@
 import { traverse } from './utils'
+import { defaultCell } from './config'
 
 describe('traverse()', () => {
   describe("when direction is 'row'", () => {
+    const grid = [new Array(9).fill({ ...defaultCell })]
+
     test('it triggers the callback for each row cell', () => {
-      expect(true).toBeTruthy()
+      const callback = jest.fn()
+      traverse('row', {
+        grid: grid,
+        coordinate: [0, 0],
+        callback
+      })
+      expect(callback.mock.calls.length).toBe(9)
     })
   })
 })
